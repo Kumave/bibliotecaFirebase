c4c1115f920add5462bd5dc6cc31d536d4ec40ec
Kumave <eli707jk@gmail.com>
2025-06-12 14:30:31 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,15 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
  const firebaseConfig = {
-    apiKey: "AIzaSyBTU",
-    authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "b19",
-    storageBucket: "bdnoapp",
-    messagingSenderId: "519",
-    appId: "1:5193611",
-    measurementId: "G0"
-  };
-
+   apiKey: "AIzaSyAN6kIzydRFze_fwhnzM-ybPHPIl2ih-QA",
+   authDomain: "bdnosql-1fc8e.firebaseapp.com",
+   projectId: "bdnosql-1fc8e",
+   storageBucket: "bdnosql-1fc8e.firebasestorage.app",
+   messagingSenderId: "638366527814",
+   appId: "1:638366527814:web:7ab3aac66736dd81e93547",
+   measurementId: "G-JXJ8F9962V"
+ };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
